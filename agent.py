f9db44d938a18e84efd5a1dcf64adc05a8617c05
Sehal Hasan <sehal.hasan16@gmail.com>
2026-09-20 12:27:53 +0530

    Created modular FIles

---
+++ agent.py
@@ -0,0 +1,11 @@
+from llm import groq_llm
+from tools import live_cricket_score
+
+
+from langchain.agents import create_agent
+
+agent = create_agent(
+    model=groq_llm,
+    system_prompt = "You are a helpful assistant",
+    tools = [live_cricket_score]
+)
---
+++ llm.py
@@ -0,0 +1,13 @@
+#LLM 
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+#Groq
+groq_llm = init_chat_model(
+    model="openai/gpt-oss-20b",
+    model_provider="groq"
+)
