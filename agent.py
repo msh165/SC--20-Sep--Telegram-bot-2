f7518ee4a88a3b1cd09b768e4fbc80ef1652524a
Sehal Hasan <sehal.hasan16@gmail.com>
2026-09-20 12:58:07 +0530

    Added RAM memory checkpointter

--- agent.py
+++ agent.py
@@ -4,8 +4,11 @@ from tools import live_cricket_score
 
 from langchain.agents import create_agent
 
+
+from langgraph.checkpoint.memory import InMemorySaver
 agent = create_agent(
     model=groq_llm,
     system_prompt = "You are a helpful assistant",
-    tools = [live_cricket_score]
+    tools = [live_cricket_score],
+    checkpointer=InMemorySaver()
 )
